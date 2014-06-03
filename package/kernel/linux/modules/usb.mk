taohaolong/openwrt

commit 0947baf57d2b207fe4c68e98dbdef377d7dfea0f

kernel: usb.mk: enable chipidea udc mode
The chipidea USB OTG controller is dual-role and supports both host and device
modes. Enable device mode.

Tested on Gateworks Ventana imx6 board with g_ether.

Are there any other targets out there that use the CHIPIDEA controller? If so
testing is needed for those.

Signed-off-by: Tim Harvey <tharvey@gateworks.com>

git-svn-id: svn://svn.openwrt.org/openwrt/trunk@40905 3c298f89-4303-0410-b956-a3cf2f4a3e73

package/kernel/linux/modules/usb.mk
@@ -1336,7 +1336,7 @@ define KernelPackage/usb-chipidea
   KCONFIG:=\
 	CONFIG_USB_CHIPIDEA \
 	CONFIG_USB_CHIPIDEA_HOST=y \
-	CONFIG_USB_CHIPIDEA_UDC=n \
+	CONFIG_USB_CHIPIDEA_UDC=y \
 	CONFIG_USB_CHIPIDEA_DEBUG=y
 ifeq ($(strip $(call CompareKernelPatchVer,$(KERNEL_PATCHVER),lt,3.11.0)),1)
   FILES:=\
@@ -1347,9 +1347,10 @@ ifeq ($(strip $(call CompareKernelPatchVer,$(KERNEL_PATCHVER),lt,3.11.0)),1)
 else
   FILES:=\
 	$(LINUX_DIR)/drivers/usb/chipidea/ci_hdrc.ko \
+	$(if $(CONFIG_OF),$(LINUX_DIR)/drivers/usb/gadget/udc-core.ko) \
 	$(if $(CONFIG_OF),$(LINUX_DIR)/drivers/usb/chipidea/ci_hdrc_imx.ko) \
 	$(if $(CONFIG_OF),$(LINUX_DIR)/drivers/usb/chipidea/usbmisc_imx.ko)
-  AUTOLOAD:=$(call AutoLoad,51,ci_hdrc $(if $(CONFIG_OF),ci_hdrc_imx usbmisc_imx),1)
+  AUTOLOAD:=$(call AutoLoad,51,ci_hdrc $(if $(CONFIG_OF),ci_hdrc_imx usbmisc_imxudc-core),1)
 endif
   $(call AddDepends/usb)
 endef

commit 0f14219a3d125a068619f87a16090b83569a75a8

kernel: fix kernel build for chipidea w/o usb-gadget
This resolves a module dependency kernel regression in r40905 with the
usb-chipidea module if usb-gadget was not selected.

Signed-off-by: Tim Harvey <tharvey@gateworks.com>

package/kernel/linux/modules/usb.mk
@@ -1373,10 +1373,10 @@ ifeq ($(strip $(call CompareKernelPatchVer,$(KERNEL_PATCHVER),lt,3.11.0)),1)
 else
   FILES:=\
 	$(LINUX_DIR)/drivers/usb/chipidea/ci_hdrc.ko \
-	$(if $(CONFIG_OF),$(LINUX_DIR)/drivers/usb/gadget/udc-core.ko) \
+	$(if $(CONFIG_USB_GADGET),$(LINUX_DIR)/drivers/usb/gadget/udc-core.ko) \
 	$(if $(CONFIG_OF),$(LINUX_DIR)/drivers/usb/chipidea/ci_hdrc_imx.ko) \
 	$(if $(CONFIG_OF),$(LINUX_DIR)/drivers/usb/chipidea/usbmisc_imx.ko)
-  AUTOLOAD:=$(call AutoLoad,51,ci_hdrc $(if $(CONFIG_OF),ci_hdrc_imx usbmisc_imxudc-core),1)
+  AUTOLOAD:=$(call AutoLoad,51,ci_hdrc $(if $(CONFIG_OF),ci_hdrc_imx usbmisc_imx),1)
 endif
   $(call AddDepends/usb)
 endef